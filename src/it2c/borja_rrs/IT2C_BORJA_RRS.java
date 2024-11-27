5b9c0ead64a6a9a49125830b2b351130cc02b302
AdolfXTR <adolfborja132@gmail.com>
2024-11-27 14:48:16 +0800

    Need Validations og limpyohanan

--- src/it2c/borja_rrs/IT2C_BORJA_RRS.java
+++ src/it2c/borja_rrs/IT2C_BORJA_RRS.java
@@ -3,17 +3,13 @@ package it2c.borja_rrs;
 import static java.lang.System.exit;
 import java.util.Scanner;
 
- 
-
 public class IT2C_BORJA_RRS {
-    
-  
+
     public static void main(String[] args) {
-        
-        
-       Scanner sc = new Scanner(System.in);
+
+        Scanner sc = new Scanner(System.in);
         String response;
-        boolean exit = true; 
+        boolean exit = true;
         do {
 
             System.out.println("-------------------------------------");
@@ -23,54 +19,56 @@ public class IT2C_BORJA_RRS {
             System.out.println("2. TABLES");
             System.out.println("3. RESERVATION");
             System.out.println("4. REPORTS");
-            
+
             System.out.println("5. EXIT");
 
             System.out.print("Enter action: ");
             int act = sc.nextInt();
-            
-
 
             switch (act) {
 
                 case 1:
-                    
-                    Customer cstm = new Customer();
-                    
-                    cstm.customerOp();
 
-                    
+                    Customer cstm = new Customer();
+
+                    cstm.customerOp();
 
                     break;
 
                 case 2:
-                    
-                    Tables tbl  = new Tables();
-                   
-                  tbl.tablesop();
-                   break;   
-                  
-                case 3:
-                    
-                  Reservations rs = new Reservations ();
-                  rs.ReservationsMenu();
+
+                    Tables tbl = new Tables();
+
+                    tbl.tablesop();
                     break;
 
-                
+                case 3:
+
+                    Reservations rs = new Reservations();
+                    rs.ReservationsMenu();
+                    break;
 
                 case 4:
+
+                    Reports rps = new Reports();
+                    
+                    rps.Report();
+                            
+                    break;
+
+                case 5:
                     System.out.println("Exiting the program. Type 'yes to coninue: !");
                     response = sc.next();
-                    if(response.equalsIgnoreCase("yes")){
+                    if (response.equalsIgnoreCase("yes")) {
                         exit = false;
                     }
                     break;
-                
+
             }
+        }
+            while (exit);
+        }
+        }
 
-        }while(exit);
-       
-    }
 
-}
-        
+
--- src/it2c/borja_rrs/Reports.java
+++ src/it2c/borja_rrs/Reports.java
@@ -1,119 +1,112 @@
-
 package it2c.borja_rrs;
 
 import java.util.Scanner;
 
- class Reports {
-     Config conf = new Config();
-    
-       public void generalReport() {
-        String query = "SELECT o_id, lname, o_due, o_date FROM tbl_order "
-                     + "LEFT JOIN customer ON customer.id = tbl_order.id "
-                     + "LEFT JOIN product ON product.p_id = tbl_order.p_id";
-        
-        String[] headers = {"Order ID", "Customer Last Name", "Due Amount", "Order Date"};
-        String[] columns = {"o_id", "lname", "o_due", "o_date"};
-        
-        System.out.println("=== GENERAL ORDER REPORT ===");
-        conf.viewRecords(query, headers, columns);
+class Reports {
+
+    Config conf = new Config();
+
+    public void generalReport() {
+        Config conf = new Config();
+
+        // Reservation Summary
+        String reservationSummaryQuery = "SELECT COUNT(*) AS total_reservations, AVG(num_people) AS avg_people FROM Reservations";
+        String[] reservationSummaryHeaders = {"Total Reservations", "Average Number of People"};
+        String[] reservationSummaryColumns = {"total_reservations", "avg_people"};
+        conf.viewRecords(reservationSummaryQuery, reservationSummaryHeaders, reservationSummaryColumns);
+
+        // Table Usage Report
+        String tableUsageQuery = "SELECT table_id, COUNT(*) AS times_reserved FROM Reservations GROUP BY table_id";
+        String[] tableUsageHeaders = {"Table ID", "Times Reserved"};
+        String[] tableUsageColumns = {"table_id", "times_reserved"};
+        conf.viewRecords(tableUsageQuery, tableUsageHeaders, tableUsageColumns);
+
+        // Customer Demographics
+        String customerDemographicsQuery = "SELECT COUNT(*) AS total_customers FROM Customer";
+        String[] customerDemographicsHeaders = {"Total Customers"};
+        String[] customerDemographicsColumns = {"total_customers"};
+        conf.viewRecords(customerDemographicsQuery, customerDemographicsHeaders, customerDemographicsColumns);
+
+        // Reservation Trends
+        String reservationTrendsQuery = "SELECT DATE(reserved_date) AS date, COUNT(*) AS total_reservations FROM Reservations GROUP BY DATE(reserved_date)";
+        String[] reservationTrendsHeaders = {"Date", "Total Reservations"};
+        String[] reservationTrendsColumns = {"date", "total_reservations"};
+        conf.viewRecords(reservationTrendsQuery, reservationTrendsHeaders, reservationTrendsColumns);
     }
 
-   
-    public void specificReport() {
+    public void individualReport() {
         Scanner sc = new Scanner(System.in);
-        
-     
-        Customer Burja = new Customer();
-        System.out.println("=== CUSTOMER LIST ===");
-        Burja.viewCustomers();
-        
-       
-        System.out.print("Enter Customer ID for specific report: ");
+        Config conf = new Config();
+
+        System.out.print("Enter Customer ID for Individual Report: ");
         int customerId = sc.nextInt();
 
-      
-        String checkCustomerQuery = "SELECT id FROM customer WHERE id = ?";
-        if (conf.getSingleValue(checkCustomerQuery, customerId) == 0) {
-            System.out.println("Customer does not exist.");
-            return;
-        }
+        // Customer Details
+        String customerQuery = "SELECT * FROM Customer WHERE customer_id = " + customerId;
+        String[] customerHeaders = {"Customer ID", "First Name", "Last Name", "Address", "Contact Number"};
+        String[] customerColumns = {"customer_id", "fname", "lname", "address", "contactnum"};
+        conf.viewRecords(customerQuery, customerHeaders, customerColumns);
 
-       
-        String query = "SELECT o_id, fname, p_name, o_due, o_date, o_status FROM tbl_order " 
-                + "LEFT JOIN customer ON customer.id = tbl_order.id " 
-                + "LEFT JOIN product ON product.p_id = tbl_order.p_id " 
-                + "WHERE tbl_order.id = ?";
-        
-        String[] hdrs = {"Order ID", "Customer's Name", "Product Name", "Due Amount", "Order Date", "Order Status"};
-        String[] colmns = {"o_id",     "fname",      "p_name",      "o_due",       "o_date",     "o_status"};
-        
-        System.out.println("=== SPECIFIC REPORT FOR CUSTOMER ID: " + customerId + " ===");
-        conf.viewRecords(query, hdrs, colmns, customerId);
+        // Reservation History
+        String reservationHistoryQuery = "SELECT * FROM Reservations WHERE customer_id = " + customerId;
+        String[] reservationHistoryHeaders = {"Reservation ID", "Customer Name", "Contact", "Number of People", "Reservation Seaters"};
+        String[] reservationHistoryColumns = {"reservation_id", "customer_name", "contactnum", "num_people", "reservation_seaters"};
+        conf.viewRecords(reservationHistoryQuery, reservationHistoryHeaders, reservationHistoryColumns);
     }
 
-    
-    public void Report(){
-        
+    public void Report() {
+
         Scanner sc = new Scanner(System.in);
         String response;
-        
-     do{    
-         System.out.println("-----------------------");
-         System.out.println("   ORDER REPORTS    ");
-         System.out.println("-----------------------");
-         System.out.println("1.GENERALALIZE REPORTS");
-         System.out.println("2.SPECIFIC REPORTS");
-         System.out.println("3.EXIT REPORTS PANEL");
-         
-         System.out.print("Enter action:");
-         int action = sc.nextInt();
-         
-         Reports rep = new Reports();
-         
-          switch(action){
-                
-               
+
+        do {
+            System.out.println("-----------------------");
+            System.out.println("   ORDER REPORTS    ");
+            System.out.println("-----------------------");
+            System.out.println("1.GENERAL REPORTS");
+            System.out.println("2.INDIVIDUAL REPORTS");
+            System.out.println("3.EXIT REPORTS PANEL");
+
+            System.out.print("Enter action:");
+            int action = sc.nextInt();
+
+            Reports rep = new Reports();
+
+            switch (action) {
+
                 case 1:
-                    
+
                     rep.generalReport();
-                    
-                    
-                    
+
                     break;
-                    
+
                 case 2:
-                   
-                    rep.specificReport();
-                              
-                    
+
+                    rep.individualReport();
+
                     break;
-                    
+
                 case 3:
-                    
-                  
+
                     System.out.println("\nReturning to Main System...\n");
                     return;
-                    
-                    default:
+
+                default:
                     System.out.print("Invalid option. Please try again.");
                     break;
             }
-            
-                     System.out.println("Do you want to continue? (yes/no): ");
+
+            System.out.println("Do you want to continue? (yes/no): ");
             response = sc.next();
 
-   
-    while (!response.equalsIgnoreCase("yes") && !response.equalsIgnoreCase("no")) {
-        System.out.println("Invalid input! Please answer only 'yes' or 'no'.");
-        System.out.print("Do you want to continue? (yes/no): ");
-        response = sc.next();
+            while (!response.equalsIgnoreCase("yes") && !response.equalsIgnoreCase("no")) {
+                System.out.println("Invalid input! Please answer only 'yes' or 'no'.");
+                System.out.print("Do you want to continue? (yes/no): ");
+                response = sc.next();
+            }
+
+        } while (response.equalsIgnoreCase("yes"));
+
     }
 
-} while (response.equalsIgnoreCase("yes"));
-        
-        
-    
-     }
-    
 }
- 
